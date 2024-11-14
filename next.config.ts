08dc55f6cc3434db348826863b029f194244fec7
Henrik Berggren <henrikadmin@henrik.berggren>
2024-11-14 17:34:55 -0500

    Opt out of eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // This completely disables ESLint during build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
